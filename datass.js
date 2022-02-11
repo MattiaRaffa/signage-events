c0c184786677d957e71d53b6123276ec9b97f5c8
Raffons <mattia.raffa@gmail.com>
2022-02-11 17:58:56 +0100

    Add files via upload

---
+++ datass.js
@@ -0,0 +1,17 @@
+const titlelss = [
+    "novarajazz",
+    "vinokilo - vintage al kilo",
+    "BMW"
+];
+
+const datass = [
+    "merc 25",
+    "ven-sab-dom 18-20",
+    "24 boh"
+];
+
+const textss = [
+    "I sondaggi geognostici permettono di prelevare carote di terreno a intervalli di 1m circa ad ogni prelievo, queste carote vengono ordinate dentro ad una cassett",
+    "ven-sab-dom 18-20",
+    "24 boh"
+];
